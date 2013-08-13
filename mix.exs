3ac953642d2afe8d2c43a2bf5ca29fceca6b153a
Dan McAulay <dmcaulay@gmail.com>
2013-08-13 00:31:41 -0700

    remove comments in the mix file

--- mix.exs
+++ mix.exs
@@ -8,13 +8,10 @@ defmodule MongoDB.Mixfile do
       deps: deps ]
   end
 
-  # Configuration for the OTP application
   def application do
     [ applications: [:bson, :mongodb] ]
   end
 
-  # Returns the list of dependencies in the format:
-  # { :foobar, "~> 0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
     [
       { :'mongodb', "v0.3.2", git: "https://github.com/dmcaulay/mongodb-erlang.git", branch: "refactor", tag: "v0.3.2" }
